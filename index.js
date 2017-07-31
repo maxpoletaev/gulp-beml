24866e2340679b396ba02a0bc094a02de6d581c0
yunusga <yunusga@yandex.ru>
2017-07-31 18:29:01 +0500

    Более подробный отчёт об ошибке

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-var map = require('map-stream');
-var beml = require('beml');
+const path = require('path');
+const map = require('map-stream');
+const beml = require('beml');
+const gutil = require('gulp-util');
+const through = require('through2');
 
-module.exports = function(config) {
+module.exports = (opts) => {
 
-  function bemlStream(file, callback) {
-    if (!file.isNull()) {
-      var html = beml(file.contents, config);
-      file.contents = new Buffer(html);
-      callback(null, file);
-    }
-  }
+    return through.obj(function (file, enc, cb) {
+        if (file.isNull()) {
+            cb(null, file);
+            return;
+        }
 
-  return map(bemlStream);
+        if (file.isStream()) {
+            cb(new gutil.PluginError('gulp-beml', 'Streaming not supported'));
+            return;
+        }
 
+        try {
+            file.contents = new Buffer(beml(file.contents.toString(), opts));
+            this.push(file);
+        } catch (err) {
+
+            Object.assign(err, {
+                filePath: file.path,
+                fileName: path.basename(file.path),
+                message: `${err.message} in (${path.basename(file.path)})`
+            });
+
+            this.emit('error', new gutil.PluginError('gulp-beml', err));
+        }
+
+        cb();
+    });
 };
